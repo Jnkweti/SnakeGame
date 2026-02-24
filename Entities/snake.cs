fd325307f2eef0ad0ce3365a94b7f67e41b586c8
Josh <joshuanyong@yahoo.com>
2026-02-24 14:41:42 -0500

    implement snake movement, direction queuing, and self-collision
    Added constructor to spawn snake at a position with a given length.
    QueueDirection blocks reversals (can't go directly opposite).
    tick() advances the snake by inserting a new head and optionally
    dropping the tail. CollideWithSelf checks if the head overlaps any
    body segment.

--- Entities/snake.cs
+++ Entities/snake.cs
@@ -4,13 +4,58 @@ using System.Collections.Generic;
 
 using System.Linq;
 
+
+
 internal sealed class SnakeEntity
 {
     public List<Coord> Body { get; } = new();
 
+    public Coord PreviousHead { get; private set; }
+
     public Direction CurrentDirection { get; private set; } = Direction.Right;
 
     private Direction _nextDirection = Direction.Right;
+
+    public SnakeEntity(Coord startPos, int initialLength = 3)
+    {
+        for (int i = 0; i < initialLength; i++)
+            Body.Add(new Coord(startPos.X - i, startPos.Y));
+
+        PreviousHead = new Coord(Body[0]);
+    }
+
+    public void QueueDirection(Direction d)
+    {
+        if(!IsOpposite(d, CurrentDirection))
+            _nextDirection = d;
+    }
+
+    public void tick(bool growing)
+    {
+        PreviousHead = new Coord(Body[0]);
+        CurrentDirection = _nextDirection;
+
+        Coord newHead = new Coord(Body[0]);
+        newHead.ApplyMovementDirection(CurrentDirection);
+        Body.Insert(0, newHead);
+
+        if (!growing)
+            Body.RemoveAt(Body.Count - 1);
+
+    }
+
+    public bool  CollideWithSelf()
+    {
+        return Body.Skip(1).Any(c => c.Equals(Body[0]));
+    }
+
+    private static bool IsOpposite(Direction a, Direction b) =>
+      (a == Direction.Left  && b == Direction.Right) ||
+      (a == Direction.Right && b == Direction.Left)  ||
+      (a == Direction.Up    && b == Direction.Down)  ||
+      (a == Direction.Down  && b == Direction.Up);
+
+
 
     
 }
